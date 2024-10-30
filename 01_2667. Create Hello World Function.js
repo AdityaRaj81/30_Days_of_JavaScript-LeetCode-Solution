3696403b1c5df2f480f66a857657ad7f60148122
Mr. Aditya <Connect2RajAditya@gmail.com>
2024-10-30 10:51:33 +0530

    Update 01_2667. Create Hello World Function.js

--- 01_2667. Create Hello World Function.js
+++ 01_2667. Create Hello World Function.js
@@ -14,3 +14,15 @@ var createHelloWorld = function() {
  * const f = createHelloWorld();
  * f(); // "Hello World"
  */
+
+                /**
+                OUTPUT
+                Input: args = []
+                Output: "Hello World"
+                Explanation:
+                const f = createHelloWorld();
+                f(); // "Hello World"
+                
+                */
+
+The function returned by createHelloWorld should always return "Hello World".
