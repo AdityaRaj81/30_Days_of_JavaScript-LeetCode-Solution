2025467de74be22ecf677f32bf151a03e40155e7
Mr. Aditya <Connect2RajAditya@gmail.com>
2024-10-31 16:09:56 +0530

    Update 04_2665. Counter II.js

--- 04_2665. Counter II.js
+++ 04_2665. Counter II.js
@@ -26,3 +26,14 @@ var createCounter = function(init) {
  * console.log(counter.reset()); // 5
  * console.log(counter.decrement()); // 4
  */
+
+            /**
+            OUTPUT
+            Input: init = 5, calls = ["increment","reset","decrement"]
+            Output: [6,5,4]
+            Explanation:
+            const counter = createCounter(5);
+            counter.increment(); // 6
+            counter.reset(); // 5
+            counter.decrement(); // 4
+            */
